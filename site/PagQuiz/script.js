84c04d2d9ec5c7c72201c2dfcff72632ad43ee71
Rogerio Cunha <rogerioleite.cunha@outlook.com>
2023-04-14 21:36:36 -0300

    Organização css e Inclusão de texto questões.

--- site/PagQuiz/script.js
+++ site/PagQuiz/script.js
@@ -4,20 +4,29 @@
 :::::::::::::::::::::::::::::::::::::::::::::::::::
 */
 
-let pergunta01 = ["Pergunta 1:", 1.1, 1.2, 1.3, 1.4, 0];
-let pergunta02 = ["Pergunta 2:", 2.1, 2.2, 2.3, 2.4, 1];
-let pergunta03 = ["Pergunta 3:", 3.1, 3.2, 3.3, 3.4, 2];
-let pergunta04 = ["Pergunta 4:", 4.1, 4.2, 4.3, 4.4, 3];
-let pergunta05 = ["Pergunta 5:", 5.1, 5.2, 5.3, 5.4, 4];
-let pergunta06 = ["Pergunta 6:", 6.1, 6.2, 6.3, 6.4, 5];
-let pergunta07 = ["Pergunta 7:", 7.1, 7.2, 7.3, 7.4, 6];
-let pergunta08 = ["Pergunta 8:", 8.1, 8.2, 8.3, 8.4, 7];
-let pergunta09 = ["Pergunta 9:", 9.1, 9.2, 9.3, 9.4, 8];
-let pergunta10 = ["Pergunta 10:", 10.1, 10.2, 10.3, 10.4, 9];
+let pergunta01 = ["No Scrum dividimos o projetos, como são chamadas essas divisões?", "Sprints.", "Product Backlog.", "Daily Scrum.", "Product Owner.", 0];
+
+let pergunta02 = ["Quando um Sprint termina? ","Quando todos os itens do Product Backlog atingem a sua definição 'Feito' no quadro Kanbam." ,"Ao expirar o tempo definido. " ,"Quando todas as tarefas foram concluídas. " , "Quando o PO diz que ela chegou ao fim.", 1];
+
+let pergunta03 = ["Qual o objetivo das Daily Scrum?", "Identificar o que funcionou bem, o que pode ser melhorado e que ações serão tomadas para melhorar.", "Reuniões longas para se discutir o projeto, sendo realizadas duas a três vezes na semana.", "Ela tem como objetivo disseminar conhecimento sobre o que foi feito no dia anterior, identificar impedimentos e priorizar o trabalho a ser realizado no dia que se inicia.", "Revisar o projeto e realizar testes.", 2];
+
+let pergunta04 = ["Quem deve saber mais sobre o progresso em direção a um objetivo de negócio ou um lançamento, e ser capaz de explicar as alternativas mais claramente?", "O Product Owner.", "A equipe de desenvolvimento.", "O Gerente de Projeto.", "O Scrum Master. ", 3];
+
+let pergunta05 = ["Qual afirmação melhor descreve a 'Sprint Retrospective'? ", " É quando a Equipe e as partes interessadas inspecionam o resultado da Sprint e descobrem o que fazer na próximo Sprint. ", "É uma revisão das atividades da equipe durante o Sprint.", "É uma demonstração no final da Sprint para todos na organização para fornecer feedback sobre o trabalho realizado. ", "Ela é usada para felicitar a equipe de desenvolvimento com ituito de  punir os membros que não cumpriram seus compromissos.", 4];
+
+let pergunta06 = ["Quem tem a última palavra sobre a ordem do Product Backlog?", "Os Stakeholders.", "O Scrum Master.", "A Equipe de Desenvolvimento.", "O Product Owner.", 5];
+
+let pergunta07 = ["De acordo com o Guia do Scrum (Scrum.org), como é constituído um Time Scrum?", "Project Manager, Agile Master e Administrator.", "Agile coordinator, Scrum e Lean.", "Product Owner, Scrum Master e Developers.", "Sprint, Portfolio manager e Stakeholders.", 6];
+
+let pergunta08 = ["A metodologia Scrum estabelece vários papéis a serem desempenhados pelo time. O responsável por controlar o progresso do desenvolvimento do projeto e ser o guardião dos ritos é o:", "product owner. ", "patrocinador do projeto.","stakeholder.", "scrum master.", 7];
+
+let pergunta09 = ["São artefatos do Scrum:", "Sprint Backlog, Product Backlog, Increment.", 'Sprint Planning, Sprint Review, Sprint Retrospective.', 'Sprint, Product Backlog, Sprint Review.', 'Sprint Planning, Daily Scrum, Increment.', 8];
+
+let pergunta10 = ["No contexto do SCRUM, a evolução diária da quantidade de atividades por fazer e da quantidade de atividades concluídas pode ser visualizada no diagrama:", "backlog scrum.", "backlog burndown.", "daily scrum.", "sprint burndown.", 9];
 
 var lista;
-var resposta = { 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0 };
-var gabarito = { 0: 0.1, 1: 1.2, 2: 2.3, 3: 3.4, 4: 4.1, 5: 5.2, 6: 6.3, 7: 7.4, 8: 8.1, 9: 9.2 };
+var resposta = { 0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0 };
+var gabarito = { 0:0.1, 1:1.2, 2:2.3, 3:3.4, 4:4.1, 5:5.2, 6:6.3, 7:7.4, 8:8.1, 9:9.2 };
 var resultado = 0;
 
 /* 
@@ -76,16 +85,15 @@ function answerTest() {
       resultado += 1;
     }
   }
-  //console.log(resultado);
+
+  console.log(resultado);
+
   document.getElementsByClassName("quizz")[0].style.display = "none";
   document.getElementsByClassName("quizz")[1].style.display = "none";
   document.getElementsByClassName("diploma")[0].style.display = "block";
   document.getElementsByClassName("diploma2")[0].style.display = "block";
- 
-  if (resultado < 6){
-  document.getElementById("nota_diploma").textContent = `Nota avaliação : ${resultado} de 10`;
-  }
 
+  document.getElementById("nota_diploma").textContent = `Nota avaliação : ${resultado} de 10`;
 }
 
 function teste() {
